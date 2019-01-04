ab20eb8dffb0b26c6de61d8b69801cd2e10442dd
nagarathna.manjunath <nagarathna.manjunath@attra.com.au>
2019-01-04 18:23:14 +0530

    Change in the notification list name .

--- app/src/main/java/com/attra/attralive/adapter/NotificationAdapter.java
+++ app/src/main/java/com/attra/attralive/adapter/NotificationAdapter.java
@@ -1,8 +1,13 @@
 package com.attra.attralive.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +16,22 @@ import android.widget.TextView;
 
 import com.attra.attralive.R;
 import com.attra.attralive.model.Notification;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
 public class NotificationAdapter extends  RecyclerView.Adapter<NotificationAdapter.productViewHolder>
 {
     private Context mcntx;
-    private List<Notification> productList;
+    private List<Notification> notiList;
+    ImageView commentnotifyDp;
 
 
 
-    public NotificationAdapter(Context mcntx, List<Notification> productList) {
+    public NotificationAdapter(Context mcntx, List<Notification> notifyList) {
         this.mcntx = mcntx;
-        this.productList = productList;
+        this.notiList = notifyList;
     }
 
     @NonNull
@@ -37,26 +45,45 @@ public class NotificationAdapter extends  RecyclerView.Adapter<NotificationAdapt
 
     @Override
     public void onBindViewHolder(@NonNull productViewHolder productViewHolder, int i) {
-        Notification notification = productList.get(i);
+        Notification notification = notiList.get(i);
+        String str = "<b>"+notification.getUserName()+"</b> has"+notification.getAction()+"on your post";
+        productViewHolder.textViewTitle.setText(Html.fromHtml(str));
+        productViewHolder.textViewShortDesc.setText("2 days ago");
+        Picasso.with(mcntx).load("https://dsd8ltrb0t82s.cloudfront.net/NewsFeedsPictures/1546592733408-29a82067b71bd9e3df95e1c0ba5c4daf--fantasy-art-avatar-jake-sully.jpg").fit().into(commentnotifyDp);
+       /* Picasso.with(mcntx).load("https://dsd8ltrb0t82s.cloudfront.net/NewsFeedsPictures/1546592733408-29a82067b71bd9e3df95e1c0ba5c4daf--fantasy-art-avatar-jake-sully.jpg").into(new Target(){
 
-        //binding the data with the viewholder views
-        productViewHolder.textViewTitle.setText(notification.getTitle());
-        productViewHolder.textViewShortDesc.setText(notification.getShortdesc());
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                commentnotifyDp.setBackground(new BitmapDrawable(mcntx.getResources(), bitmap));
+            }
+
+            @Override
+            public void onBitmapFailed(final Drawable errorDrawable) {
+                Log.d("TAG", "FAILED");
+            }
+
+            @Override
+            public void onPrepareLoad(final Drawable placeHolderDrawable) {
+                Log.d("TAG", "Prepare Load");
+            }
+        });*/
 
     }
 
     @Override
     public int getItemCount() {
-        return productList.size();
+        return notiList.size();
     }
 
     class productViewHolder extends RecyclerView.ViewHolder
     {
-        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
-        ImageView imageView;
+        TextView textViewTitle, textViewShortDesc;
+
 
         public productViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            commentnotifyDp = itemView.findViewById(R.id.commentNotifyDp);
             textViewTitle = itemView.findViewById(R.id.textViewTitle);
             textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
 
