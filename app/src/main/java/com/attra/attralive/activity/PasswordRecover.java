6f98698ba8f6cbf46237bb80f683585f7535d779
Awnish Kumar(Attra) <awnish.kumar@attra.com.au>
2019-01-05 13:38:24 +0530

    added forgot password and reset password screen

---
+++ app/src/main/java/com/attra/attralive/activity/PasswordRecover.java
@@ -0,0 +1,13 @@
+package com.attra.attralive.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswordRecover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recover);
+    }
+}
