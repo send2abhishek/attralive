02e30e4ee788b0d27b96fec48d92bbd3e41af666
Awnish Kumar(Attra) <awnish.kumar@attra.com.au>
2019-01-14 19:41:51 +0530

    reset password issue fix

--- app/src/main/java/com/attra/attralive/Service/MyAppolloClient.java
+++ app/src/main/java/com/attra/attralive/Service/MyAppolloClient.java
@@ -10,10 +10,8 @@ import okhttp3.logging.HttpLoggingInterceptor;
 
 public class MyAppolloClient {
 
-
-
-    private static final String URL = "http://13.232.225.201:80/graphql";
-    //   private static  final String URL = "http://10.200.44.23:4001/graphql";
+    //private static final String URL = "http://13.232.225.201:80/graphql";
+       private static  final String URL = "http://10.200.44.23:4001/graphql";
 
 
 
--- app/src/main/java/com/attra/attralive/activity/UserDetailsActivity.java
+++ app/src/main/java/com/attra/attralive/activity/UserDetailsActivity.java
@@ -3,46 +3,30 @@ package com.attra.attralive.activity;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Parcelable;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-
-import android.graphics.Bitmap;
-import android.net.Uri;
-
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
@@ -65,7 +49,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -75,7 +58,7 @@ import javax.annotation.Nonnull;
 import fr.ganfra.materialspinner.MaterialSpinner;
 import graphqlandroid.GetBusinessUnit;
 import graphqlandroid.GetLocation;
-
+import graphqlandroid.UserDetailsUpdate;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -88,8 +71,6 @@ import retrofit2.Retrofit;
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
-import graphqlandroid.UserDetailsUpdate;
 
 
 public class UserDetailsActivity extends AppCompatActivity {
@@ -571,7 +552,8 @@ public class UserDetailsActivity extends AppCompatActivity {
 
 
     }
-    public void subscribeToTopic(String location){
+
+    public void subscribeToTopic(String location) {
         FirebaseMessaging.getInstance().subscribeToTopic(location)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -581,13 +563,11 @@ public class UserDetailsActivity extends AppCompatActivity {
                             msg = getString(R.string.msg_subscribe_failed);
                         }
                         Log.d("topic subscription", msg);
-                      //  Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        //  Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 });
 
     }
-
-
 
 
 }
